4079fa6bfb7791f5b5dac904f7b620f529d272bb
Hady Mashhour <63734405+HadyMash@users.noreply.github.com>
2021-11-26 16:36:40 +0400

    Updated the rotation of the player based on their velocity.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] readonly private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float lerpMultiplier = 15;
+    [SerializeField] private float velocityMultiplier = 2f;
     public float jumpForce = 5f;
+
+    private void Start()
+    {
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.LerpAngle(transform.rotation.eulerAngles.z, Mathf.Min(Mathf.Max(rb.velocity.y * velocityMultiplier, -90), 90), Time.deltaTime * lerpMultiplier));
+    }
 
     private void OnJump()
     {
